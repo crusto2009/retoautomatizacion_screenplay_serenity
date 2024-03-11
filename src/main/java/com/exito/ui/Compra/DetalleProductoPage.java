22678fa3a74cf7bf2c4d5779f81aa799aa6d9a08
CristianAlbertoHerná <cristian.hernandeza@sqasa.co>
2024-03-11 12:20:54 -0500

    Subiendo archivo Jenkisfile

--- src/main/java/com/exito/ui/Compra/DetalleProductoPage.java
+++ src/main/java/com/exito/ui/Compra/DetalleProductoPage.java
@@ -7,6 +7,6 @@ public class DetalleProductoPage {
 
     public static final Target BTN_AGREGAR_CARRITO = Target.the("Boton agregar al carrito").located(By.xpath("//button//span[.='Agregar']"));
 
-    public static final Target BTN_CARRITO = Target.the("Boton carrito compra").located(By.xpath("//a[@class='exito-header-3-x-minicartLink']"));
+    public static final Target BTN_CARRITO = Target.the("Boton carrito compra").located(By.xpath("//*[@id='__next']/header/section/div/div[2]/div[2]/button"));
 
 }
